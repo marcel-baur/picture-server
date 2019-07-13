8597b70f9ea14308eb260e22db52879f1674225e
Marcel Baur <marcel@appdafuer.com>
2019-07-13 14:16:29 +0200

    Add multiple file upload support

--- main/server.go
+++ main/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"strings"
@@ -10,7 +11,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func UploadSeveral(w http.ResponseWriter, r *http.Request) {
+func getExtension(f *multipart.FileHeader) string {
+	return strings.Split(f.Filename, ".")[1]
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+
 	err := r.ParseMultipartForm(100000)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -20,91 +26,47 @@ func UploadSeveral(w http.ResponseWriter, r *http.Request) {
 	m := r.MultipartForm
 
 	files := m.File["myFile"]
-	for i, _ := range files {
+
+	for i := range files {
+		fmt.Println("server.uploadFile")
 		file, err := files[i].Open()
+
+		if err != nil {
+			fmt.Println("Error retrieving the file")
+			fmt.Println(err)
+			return
+		}
 		defer file.Close()
+		extension := getExtension(files[i])
+
+		fileBytes, err := ioutil.ReadAll(file)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			fmt.Println(err)
+		}
+
+		filepath := files[i].Filename
+		f, err := os.Create(filepath)
+		f.Write(fileBytes)
+		f.Close()
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
-		// stat, err := os.Stat(files[i].Filename)
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	return
-		// }
-		// subfolder := strings.Split( stat.ModTime().String(), " ")[0]
-		err = os.Mkdir("img", os.ModePerm)
+
+		fmt.Fprintf(w, "Successfully Uploaded File\n")
+		stat, err := os.Stat(files[i].Filename)
 		if err != nil {
-			fmt.Println("here1")
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			fmt.Println("Error reading file Stat")
+			fmt.Println(err)
 		}
-		err = os.Rename(files[i].Filename, "img/"+files[i].Filename)
-		if err != nil {
-			fmt.Println("here")
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// dst, err := os.Create("/img/upload/"+files[i].Filename)
-		// defer dst.Close()
-		// if err != nil {
-		// 	fmt.Println("here")
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	return
-		// }
-		// if _, err := io.Copy(dst, file); err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	return
-		// }
-
-	}
-	fmt.Fprintf(w, "Successfully Uploaded %v File(s)\n", len(files))
-}
-
-func Upload(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("server.uploadFile")
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error retrieving the file")
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-	fmt.Println("Uploaded File: %v\n", handler.Filename)
-	fmt.Println("File Size: %v\n", handler.Size)
-	fmt.Println("MIME Header: %v\n", handler.Header)
-
-	extension := strings.Split(handler.Filename, ".")[1]
-	fmt.Println(extension)
-
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
+		dirname := strings.Split(stat.ModTime().String(), " ")[0]
+		fmt.Println(dirname)
+		os.Mkdir(dirname, os.ModePerm)
+		destination := dirname + "/" + extension
+		os.Mkdir(destination, os.ModePerm)
+		err = os.Rename(filepath, destination+"/"+filepath)
 		fmt.Println(err)
 	}
-
-	filepath := handler.Filename
-	f, err := os.Create(filepath)
-	f.Write(fileBytes)
-	f.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-	stat, err := os.Stat(handler.Filename)
-	if err != nil {
-		fmt.Println("Error reading file Stat")
-		fmt.Println(err)
-	}
-	dirname := strings.Split(stat.ModTime().String(), " ")[0]
-	fmt.Println(dirname)
-	os.Mkdir(dirname, os.ModePerm)
-	destination := dirname + "/" + extension
-	os.Mkdir(destination, os.ModePerm)
-	err = os.Rename(filepath, destination+"/"+filepath)
-	fmt.Println(err)
 }
 
 func main() {
@@ -116,7 +78,7 @@ func main() {
 
 		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
-	r.HandleFunc("/upload", UploadSeveral).Methods("POST")
+	r.HandleFunc("/upload", Upload).Methods("POST")
 	r.HandleFunc("/uploadSingle", Upload).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
