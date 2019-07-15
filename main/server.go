d14bbcac424a3ee28dcf36fa64f10d1589c695af
Marcel Baur <marcel@appdafuer.com>
2019-07-15 18:00:51 +0200

    Offload handleFileUpload

--- main/server.go
+++ main/server.go
@@ -15,6 +15,47 @@ func getExtension(f *multipart.FileHeader) string {
 	return strings.Split(f.Filename, ".")[1]
 }
 
+func handleFileUpload(index int, files []*multipart.FileHeader) {
+	fmt.Println("server.uploadFile")
+	file, err := files[index].Open()
+
+	if err != nil {
+		fmt.Println("Error retrieving the file")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	extension := getExtension(files[index])
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	filepath := files[index].Filename
+	f, err := os.Create(filepath)
+	f.Write(fileBytes)
+	f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// fmt.Fprintf(w, "Successfully Uploaded File\n")
+	stat, err := os.Stat(files[index].Filename)
+	if err != nil {
+		fmt.Println("Error reading file Stat")
+		fmt.Println(err)
+	}
+	dirname := strings.Split(stat.ModTime().String(), " ")[0]
+	fmt.Println(dirname)
+	os.Mkdir(dirname, os.ModePerm)
+	destination := dirname + "/" + extension
+	os.Mkdir(destination, os.ModePerm)
+	err = os.Rename(filepath, destination+"/"+filepath)
+	fmt.Println(err)
+}
+
 func Upload(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseMultipartForm(100000)
@@ -28,44 +69,11 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 	files := m.File["myFile"]
 
 	for i := range files {
-		fmt.Println("server.uploadFile")
-		file, err := files[i].Open()
-
-		if err != nil {
-			fmt.Println("Error retrieving the file")
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
-		extension := getExtension(files[i])
-
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		filepath := files[i].Filename
-		f, err := os.Create(filepath)
-		f.Write(fileBytes)
-		f.Close()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "Successfully Uploaded File\n")
-		stat, err := os.Stat(files[i].Filename)
-		if err != nil {
-			fmt.Println("Error reading file Stat")
-			fmt.Println(err)
-		}
-		dirname := strings.Split(stat.ModTime().String(), " ")[0]
-		fmt.Println(dirname)
-		os.Mkdir(dirname, os.ModePerm)
-		destination := dirname + "/" + extension
-		os.Mkdir(destination, os.ModePerm)
-		err = os.Rename(filepath, destination+"/"+filepath)
-		fmt.Println(err)
+		handleFileUpload(i, files)
+	}
+	_, err = fmt.Fprintf(w, "Successfully Uploaded all files")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
